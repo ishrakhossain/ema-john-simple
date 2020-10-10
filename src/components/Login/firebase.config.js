ef074093bcafb4a215d466fb1e3846d4f18ec6fb
Ishrak <ishrak.hossain789@gmail.com>
2020-10-11 01:14:06 +0600

    first

--- src/components/Login/firebase.config.js
+++ src/components/Login/firebase.config.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyBUCI-X-qfRziWDj2c0O3HriIh264eKzlA",
-    authDomain: "fire-auth-e41a2.firebaseapp.com",
-    databaseURL: "https://fire-auth-e41a2.firebaseio.com",
-    projectId: "fire-auth-e41a2",
-    storageBucket: "fire-auth-e41a2.appspot.com",
-    messagingSenderId: "909867307808",
-    appId: "1:909867307808:web:07d3712290978892f0fd45"
-  };
+  apiKey: "AIzaSyBLK_7XxkXiAQCwM5bcnnjxw7REBiH9UVs",
+  authDomain: "ema-john-adb83.firebaseapp.com",
+  databaseURL: "https://ema-john-adb83.firebaseio.com",
+  projectId: "ema-john-adb83",
+  storageBucket: "ema-john-adb83.appspot.com",
+  messagingSenderId: "941819797233",
+  appId: "1:941819797233:web:08c8ca016447aa69f65350"
+};
   
   export default firebaseConfig;
